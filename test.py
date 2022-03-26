941bd49f035c6a41e670751d2f63c2c3799df5f8
Qinx <me@qinxuzhou.com>
2022-03-26 10:35:32 -0600

    add python code and test

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+import py_code
+
+class TestMethods(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
